13d3928225f657a872594057b04deb2bb6892e97
mkarneim <karneim@macbookpro2.local>
2011-09-29 11:37:45 +0200

    printing pojos to system out

--- samples/src/main/java/Main.java
+++ samples/src/main/java/Main.java
@@ -1,19 +1,26 @@
+import java.util.Arrays;
+
 import pojos.Contact;
 import pojos.ContactBuilder;
 
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Running Sample.");
 		// Build a single contact
 		Contact james = new ContactBuilder()
 			.withName("James Bond")
 			.withEmail("007@secretservice.org")
 			.build();
 
+		System.out.println("James:\n"+james);
+		
 		// Build 100 contacts each with one of the given names and no email 
 		Contact[] someContacts = new ContactBuilder()
 				.withNameFrom("Alice", "Bob", "Charly")				
 				.buildArray( 100);
+		
+		System.out.println("Some contacts:\n"+Arrays.toString( someContacts));
 
 	}
 }
