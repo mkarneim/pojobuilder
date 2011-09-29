d47b91d9c028bc29e312edd5041b9b749714a6b1
mkarneim <karneim@macbookpro2.local>
2011-09-29 10:39:27 +0200

    Extended sample code

--- samples/src/generated/java/pojos/ContactBuilder.java
+++ samples/src/generated/java/pojos/ContactBuilder.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import java.lang.String;
-import java.util.Date;
 
 
 /**
@@ -19,11 +18,8 @@ public class ContactBuilder implements Cloneable {
 	 */
 	private int buildCount = 0;
 
-	private String[] phone$java$lang$String = new String[] {};
-	private String[] lastname$java$lang$String = new String[] {null}; // constructor parameter
-	private Date[] birthdate$java$util$Date = new Date[] {};
-	private String[] firstname$java$lang$String = new String[] {null}; // constructor parameter
 	private String[] email$java$lang$String = new String[] {};
+	private String[] name$java$lang$String = new String[] {null}; // constructor parameter
 
 	
 	/**
@@ -32,98 +28,6 @@ public class ContactBuilder implements Cloneable {
 	public ContactBuilder() {
 	}
 	
-	/**
-	 * Sets the default value for the {@link Contact#phone$java$lang$String} property.
-	 * @param value the default value
-	 * @return this builder
-	 */
-	public ContactBuilder withPhone( String value) {
-		this.phone$java$lang$String = new String[] { value };
-		return this;
-	}
-
-	/**
-	 * Sets the default values for the {@link Contact#phone$java$lang$String} property.
-	 * @param values the default values
-	 * @return this builder
-	 */
-	public ContactBuilder withPhoneFrom( String... values) {
-	if ( values == null) {
-		throw new IllegalArgumentException("array of default values must not be null!");
-	}
-		this.phone$java$lang$String = values;
-		return this;
-	}
-	/**
-	 * Sets the default value for the {@link Contact#lastname$java$lang$String} property.
-	 * @param value the default value
-	 * @return this builder
-	 */
-	public ContactBuilder withLastname( String value) {
-		this.lastname$java$lang$String = new String[] { value };
-		return this;
-	}
-
-	/**
-	 * Sets the default values for the {@link Contact#lastname$java$lang$String} property.
-	 * @param values the default values
-	 * @return this builder
-	 */
-	public ContactBuilder withLastnameFrom( String... values) {
-	if ( values == null) {
-		throw new IllegalArgumentException("array of default values must not be null!");
-	}
-	if ( values.length == 0) {
-		throw new IllegalArgumentException("array of default values for constructor parameters must contain at least one element");
-	} 	this.lastname$java$lang$String = values;
-		return this;
-	}
-	/**
-	 * Sets the default value for the {@link Contact#birthdate$java$util$Date} property.
-	 * @param value the default value
-	 * @return this builder
-	 */
-	public ContactBuilder withBirthdate( Date value) {
-		this.birthdate$java$util$Date = new Date[] { value };
-		return this;
-	}
-
-	/**
-	 * Sets the default values for the {@link Contact#birthdate$java$util$Date} property.
-	 * @param values the default values
-	 * @return this builder
-	 */
-	public ContactBuilder withBirthdateFrom( Date... values) {
-	if ( values == null) {
-		throw new IllegalArgumentException("array of default values must not be null!");
-	}
-		this.birthdate$java$util$Date = values;
-		return this;
-	}
-	/**
-	 * Sets the default value for the {@link Contact#firstname$java$lang$String} property.
-	 * @param value the default value
-	 * @return this builder
-	 */
-	public ContactBuilder withFirstname( String value) {
-		this.firstname$java$lang$String = new String[] { value };
-		return this;
-	}
-
-	/**
-	 * Sets the default values for the {@link Contact#firstname$java$lang$String} property.
-	 * @param values the default values
-	 * @return this builder
-	 */
-	public ContactBuilder withFirstnameFrom( String... values) {
-	if ( values == null) {
-		throw new IllegalArgumentException("array of default values must not be null!");
-	}
-	if ( values.length == 0) {
-		throw new IllegalArgumentException("array of default values for constructor parameters must contain at least one element");
-	} 	this.firstname$java$lang$String = values;
-		return this;
-	}
 	/**
 	 * Sets the default value for the {@link Contact#email$java$lang$String} property.
 	 * @param value the default value
@@ -144,6 +48,30 @@ public class ContactBuilder implements Cloneable {
 		throw new IllegalArgumentException("array of default values must not be null!");
 	}
 		this.email$java$lang$String = values;
+		return this;
+	}
+	/**
+	 * Sets the default value for the {@link Contact#name$java$lang$String} property.
+	 * @param value the default value
+	 * @return this builder
+	 */
+	public ContactBuilder withName( String value) {
+		this.name$java$lang$String = new String[] { value };
+		return this;
+	}
+
+	/**
+	 * Sets the default values for the {@link Contact#name$java$lang$String} property.
+	 * @param values the default values
+	 * @return this builder
+	 */
+	public ContactBuilder withNameFrom( String... values) {
+	if ( values == null) {
+		throw new IllegalArgumentException("array of default values must not be null!");
+	}
+	if ( values.length == 0) {
+		throw new IllegalArgumentException("array of default values for constructor parameters must contain at least one element");
+	} 	this.name$java$lang$String = values;
 		return this;
 	}
 
@@ -176,13 +104,7 @@ public class ContactBuilder implements Cloneable {
 	 * @return the created Contact
 	 */
 	public Contact build() {
-		Contact result = new Contact( firstname$java$lang$String[buildCount%firstname$java$lang$String.length], lastname$java$lang$String[buildCount%lastname$java$lang$String.length]);
-		if ( phone$java$lang$String.length > 0) {    
-			result.setPhone( phone$java$lang$String[buildCount%phone$java$lang$String.length]);		
-		}
-		if ( birthdate$java$util$Date.length > 0) {    
-			result.setBirthdate( birthdate$java$util$Date[buildCount%birthdate$java$util$Date.length]);		
-		}
+		Contact result = new Contact( name$java$lang$String[buildCount%name$java$lang$String.length]);
 		if ( email$java$lang$String.length > 0) {    
 			result.setEmail( email$java$lang$String[buildCount%email$java$lang$String.length]);		
 		}
