23f24f9d155e83ae5eafb44986dd636962577b38
mkarneim <mkarneim>
2011-12-29 09:53:29 +0100

    New version 1.2.0

--- samples/src/main/java/samples/with/factory/PojoFactory.java
+++ samples/src/main/java/samples/with/factory/PojoFactory.java
@@ -6,16 +6,10 @@ import net.karneim.pojobuilder.PropertyNames;
 public class PojoFactory {
 
     @GeneratePojoBuilder
-<<<<<<< HEAD
+
     @PropertyNames({ "firstname", "surname" })
     public static Contact createContact(String aFirstname, String aSurname) {
         Contact result = new Contact(aSurname, aFirstname);
-=======
-    @PropertyNames({ "lastname", "firstname" })
-    public static Contact createContact(String aLastname, String aFirstname) {
-        // reordering the arguments just for fun
-        Contact result = new Contact(aFirstname, aLastname);
->>>>>>> 1fc315b61a33b0dabfe0035a1baa79c1ad3f0c68
         return result;
     }
 
