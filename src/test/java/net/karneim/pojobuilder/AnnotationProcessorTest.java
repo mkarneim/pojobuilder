32c186e30dc5c54df251a4bc4af36084767c9e56
mkarneim <mkarneim@googlemail.com>
2012-11-24 13:58:03 +0100

    tidy up of acceptance tests

--- src/test/java/net/karneim/pojobuilder/AnnotationProcessorTest.java
+++ src/test/java/net/karneim/pojobuilder/AnnotationProcessorTest.java
@@ -1,6 +1,6 @@
 package net.karneim.pojobuilder;
 
-import net.karneim.pojobuilder.AnnotationProcessor;
+import java.lang.reflect.Modifier;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -10,102 +10,101 @@ import org.junit.Test;
 import testenv.JavaProject;
 import testenv.Util;
 
-
+/**
+ * The {@link AnnotationProcessorTest} is a simple acceptance test for running
+ * the {@link AnnotationProcessor} on pojos.
+ * 
+ */
 public class AnnotationProcessorTest {
 
 	JavaProject prj = new JavaProject(Util.createTempDir());
 
 	@Before
-	public void setupFixture() {
-		// Enable the GeneratePojoBuilderAnnotationProcessor
-		prj.getProcessorClasses().add(
-				AnnotationProcessor.class);
+	public void setupJavaProject() {
+		// Enable the AnnotationProcessor
+		prj.getProcessorClasses().add(AnnotationProcessor.class);
 		// Enable Logging
 		AnnotationProcessor.enableLogging();
 	}
 
 	@After
-	public void tearDown() {
+	public void tearDownJavaProject() {
 		prj.delete();
 	}
 
 	@Test
-	public void shouldCompileEmptyPojo() throws Exception {
-		// Given there is a pojo source file
+	public void testCompileShouldCompileEmptyPojo() throws Exception {
+		// Given: there is a pojo source file
 		prj.addSourceFile("src/test/data/testdata/EmptyPojo.java");
 
-		// When the compilation is invoked
+		// When: the compilation is invoked
 		boolean success = prj.compile();
 
-		// Then the class file is generated
-		Assert.assertTrue("success", success);
+		// Then: the class file is generated
+		Assert.assertEquals("success", true, success);
 		Class<?> pojoClass = prj.findClass("testdata.EmptyPojo");
 		Assert.assertNotNull("pojoClass", pojoClass);
 	}
 
 	@Test
-	public void shouldProduceSimpleAnnotatedPojoBuilder() throws Exception {
-		// Given there is an annotated pojo source file
+	public void testCompileShouldGeneratePojoBuilder1() throws Exception {
+		// Given: there is an annotated pojo source file
 		prj.addSourceFile("src/test/data/testdata/SimpleAnnotatedPojo.java");
 
-		// When the compilation is invoked
+		// When: the compilation is invoked
 		boolean success = prj.compile();
 
-		// Then a new builder class is generated with a matching name
-		Assert.assertTrue("success", success);
-		Class<?> newClass = prj
-				.findClass("testdata.SimpleAnnotatedPojoBuilder");
+		// Then: a new builder class is generated with a matching name
+		Assert.assertEquals("success", true, success);
+		Class<?> newClass = prj.findClass("testdata.SimpleAnnotatedPojoBuilder");
 		Assert.assertNotNull("newClass", newClass);
 	}
-	
+
 	@Test
-	public void shouldProduceContactBuilder() throws Exception {
-		// Given there is an annotated pojo source file
+	public void testCompileShouldGeneratePojoBuilder2() throws Exception {
+		// Given: there is an annotated pojo source file
 		prj.addSourceFile("src/test/data/testdata/Contact.java");
 
-		// When the compilation is invoked
+		// When: the compilation is invoked
 		boolean success = prj.compile();
 
-		// Then a new builder class is generated with a matching name
-		Assert.assertTrue("success", success);
-		Class<?> newClass = prj
-				.findClass("testdata.ContactBuilder");
+		// Then: a new builder class is generated with a matching name
+		Assert.assertEquals("success", true, success);
+		Class<?> newClass = prj.findClass("testdata.ContactBuilder");
 		Assert.assertNotNull("newClass", newClass);
 	}
-	
+
 	@Test
-	public void shouldProduceContactBuilderIntoTargetPackage() throws Exception {
-		// Given there is an annotated pojo source file
+	public void testCompileShouldGeneratePojoBuilderIntoTargetPackage() throws Exception {
+		// Given: there is an annotated pojo source file with "intoPackage" directive
 		prj.addSourceFile("src/test/data/testdata/intoPackage/Contact.java");
 
-		// When the compilation is invoked
+		// When: the compilation is invoked
 		boolean success = prj.compile();
 
-		// Then a new builder class is generated with a matching name
-		Assert.assertTrue("success", success);
-		Class<?> newClass = prj
-				.findClass("testdata.intoPackage.target.ContactBuilder");
+		// Then: a new builder class is generated into target directory
+		Assert.assertEquals("success", true, success);
+		Class<?> newClass = prj.findClass("testdata.intoPackage.target.ContactBuilder");
 		Assert.assertNotNull("newClass", newClass);
 	}
-	
+
 	@Test
-	public void shouldProduceAbstractBuilderAndSubclass() throws Exception {
-		// Given there is an annotated pojo source file
+	public void testCompileShouldGeneratePojoBuilderWithGenerationGap() throws Exception {
+		// Given: there is an annotated pojo source file with "userGenerationGap" directive
 		prj.addSourceFile("src/test/data/testdata/generationgap/Contact.java");
 
-		// When the compilation is invoked
+		// When: the compilation is invoked
 		boolean success = prj.compile();
 
-		// Then a new builder class is generated with a matching name
-		Assert.assertTrue("success", success);
-		Class<?> newClass = prj
-				.findClass("testdata.generationgap.ContactBuilder");
+		// Then: two new classes are generated, one abstract and the other concrete
+		Assert.assertEquals("success", true, success);
+		Class<?> newClass = prj.findClass("testdata.generationgap.ContactBuilder");
 		Assert.assertNotNull("newClass", newClass);
-		Class<?> newAbstractClass = prj
-				.findClass("testdata.generationgap.AbstractContactBuilder");
+		Class<?> newAbstractClass = prj.findClass("testdata.generationgap.AbstractContactBuilder");
 		Assert.assertNotNull("newAbstractClass", newAbstractClass);
-
+		Assert.assertEquals( "abstract",  true, Modifier.isAbstract(newAbstractClass.getModifiers()));
+		Assert.assertEquals( "abstract",  false, Modifier.isAbstract(newClass.getModifiers()));
+		Assert.assertEquals( "superclass",  newAbstractClass, newClass.getSuperclass());
 	}
-	
 
 }
