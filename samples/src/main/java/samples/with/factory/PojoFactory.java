381238808fedc0f6506237257f717d809ee2b62c
Marc Carter <drekbour@fastmail.fm>
2013-05-18 11:42:18 +0100

    #42 Sample for implicit properties

--- samples/src/main/java/samples/with/factory/PojoFactory.java
+++ samples/src/main/java/samples/with/factory/PojoFactory.java
@@ -5,15 +5,20 @@ import net.karneim.pojobuilder.FactoryProperties;
 
 public class PojoFactory {
 
-	@GeneratePojoBuilder
-	@FactoryProperties({ "firstname", "surname" })
-	public static Contact createContact(String aFirstname, String aSurname) {
-		Contact result = new Contact(aSurname, aFirstname);
-		return result;
+    @GeneratePojoBuilder
+    public static Note createNote() {
+        return new Note();
+    }
+
+    @GeneratePojoBuilder
+	public static Contact createContact(String firstname, String surname) {
+		return new Contact(surname, firstname);
 	}
 
-	@GeneratePojoBuilder
-	public static Note createNote() {
-		return new Note();
-	}
+    @GeneratePojoBuilder
+    @FactoryProperties({ "firstname", "surname" })
+    public static Contact construireContact(String prenom, String nomDeFamille) {
+        return new Contact(prenom, nomDeFamille);
+    }
+
 }
