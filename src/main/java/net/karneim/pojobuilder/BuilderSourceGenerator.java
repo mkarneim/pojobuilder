dd3c026422d1dde255f99f620a0dae51059d3afd
Marc Carter <drekbour@fastmail.fm>
2013-07-06 20:27:37 +0100

    Platform independent line separator in log

--- src/main/java/net/karneim/pojobuilder/BuilderSourceGenerator.java
+++ src/main/java/net/karneim/pojobuilder/BuilderSourceGenerator.java
@@ -1,30 +1,30 @@
 package net.karneim.pojobuilder;
 
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+
 import java.io.IOException;
 import java.io.Writer;
 import java.util.logging.Logger;
 
-import org.stringtemplate.v4.ST;
-import org.stringtemplate.v4.STGroup;
-
 public class BuilderSourceGenerator {
-	private static final Logger LOG = Logger.getLogger(BuilderSourceGenerator.class.getName());
-	private STGroup group;
+    private static final Logger LOG = Logger.getLogger(BuilderSourceGenerator.class.getName());
+    private STGroup group;
 
-	public BuilderSourceGenerator(STGroup group) {
-		this.group = group;
-	}
+    public BuilderSourceGenerator(STGroup group) {
+        this.group = group;
+    }
 
-	public String generate(Object model) {
-		ST template = group.getInstanceOf("builderClass");
-		template.add("model", model);
-		return template.render();
-	}
+    public String generate(Object model) {
+        ST template = group.getInstanceOf("builderClass");
+        template.add("model", model);
+        return template.render();
+    }
 
-	public void generate(Object model, Writer writer) throws IOException {
-		String text = generate(model);
-		LOG.finest(String.format("Generating source:\n%s", text));
-		writer.append(text);
-	}
+    public void generate(Object model, Writer writer) throws IOException {
+        String text = generate(model);
+        LOG.finest(String.format("Generating source:%n%s", text));
+        writer.append(text);
+    }
 
 }
