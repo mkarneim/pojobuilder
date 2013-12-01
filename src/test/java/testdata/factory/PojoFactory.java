abcac0bd13330423d811d52473ef5bc7a40589ae
mkarneim <mkarneim@googlemail.com>
2013-12-01 09:29:42 +0100

    added some reminders for additional tests

--- src/test/java/testdata/factory/PojoFactory.java
+++ src/test/java/testdata/factory/PojoFactory.java
@@ -54,4 +54,47 @@ public class PojoFactory {
     public static Note createNote() {
         return new Note();
     }
+
+    
+    // TODO test factory methods with generics.
+    
+//    @GeneratePojoBuilder(withName="EmptyMapBuilder", intoPackage="sample")
+//    public static Map createEmptyMap() {
+//        return new HashMap();
+//    }
+
+//    @GeneratePojoBuilder(withName="GenericMapBuilder", intoPackage="sample")
+//    public static <K, V> Map<K, V> createGenericMap() {
+//        return new HashMap();
+//    }
+
+//    @GeneratePojoBuilder(withName="StringIntegerMapBuilder", intoPackage="sample")
+//    public static Map<String, Integer> createStringIntegerMap() {
+//        return new HashMap();
+//    }
+    
+//    @GeneratePojoBuilder(withName="BoundedListBuilder", intoPackage="sample") 
+//    public static <T extends Number & Comparable<T>> List<T> createBoundedList() {
+//        return new ArrayList();
+//    }
+
+//    @GeneratePojoBuilder(withName="BoundedMapBuilder", intoPackage="sample")
+//    public static <T extends Number, X extends Serializable & Comparable<X>> Map<T, X> createBoundedMap() {
+//        return new HashMap();
+//    }
+    
+//    @GeneratePojoBuilder(withName="IntegerMultiSetBuilder", intoPackage="sample")
+//    public static <T extends Set<Integer>> Set<T> createIntegerMultiSet() {
+//        return new HashSet();
+//    }
+    
+//    @GeneratePojoBuilder(withName="MultiSetBuilder", intoPackage="sample") 
+//    public static <T extends Set<V>, V> Set<T> createMultiSet() {
+//        return new HashSet();
+//    }
+    
+//    @GeneratePojoBuilder(withName="BoundedMapBuilder2", intoPackage="sample") 
+//    public static <T extends Number, X extends T> Map<T, X> createBoundedMap2() {
+//        return new HashMap();
+//    }
 }
