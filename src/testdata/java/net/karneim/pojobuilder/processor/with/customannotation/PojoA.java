6c600022ab4e53af96df43b1cdb3d2eebf928d7b
Michael Karneim <mkarneim@googlemail.com>
2014-09-01 18:52:25 +0200

    Revert "Meta annotations"

--- src/testdata/java/net/karneim/pojobuilder/processor/with/customannotation/PojoA.java
+++ src/testdata/java/net/karneim/pojobuilder/processor/with/customannotation/PojoA.java
@@ -3,6 +3,7 @@ package net.karneim.pojobuilder.processor.with.customannotation;
 import net.karneim.pojobuilder.GeneratePojoBuilder;
 
 @MyCustomAnnotationA
+@GeneratePojoBuilder(intoPackage="*.builder")
 public class PojoA {
   public String name;
 }
