4d87dc116c8c1dc7e5db3e9373cde073f3f051b8
Adrodoc55 <adrodoc55@googlemail.com>
2016-12-24 23:56:52 +0100

    Fix #127

---
+++ src/testdata/java/net/karneim/pojobuilder/analysis/with/setter/PojoWithPublicObjectObjectSetter.java
@@ -0,0 +1,10 @@
+package net.karneim.pojobuilder.analysis.with.setter;
+
+import net.karneim.pojobuilder.GeneratePojoBuilder;
+
+@GeneratePojoBuilder
+public class PojoWithPublicObjectObjectSetter {
+  public Object setMyProperty(Object myProperty) {
+    return null;
+  }
+}
---
+++ src/testdata/java/net/karneim/pojobuilder/analysis/with/setter/PojoWithPublicVoidPrimitiveSetter.java
@@ -0,0 +1,8 @@
+package net.karneim.pojobuilder.analysis.with.setter;
+
+import net.karneim.pojobuilder.GeneratePojoBuilder;
+
+@GeneratePojoBuilder
+public class PojoWithPublicVoidPrimitiveSetter {
+  public void setMyProperty(int myProperty) {}
+}
